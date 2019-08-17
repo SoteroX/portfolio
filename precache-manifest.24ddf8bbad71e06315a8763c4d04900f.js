937f2098ff9500fbd790f186576d0946e1fcf96a
SoteroX <soterox13@gmail.com>
2019-08-16 19:44:57 -0500

    Updates

--- precache-manifest.24ddf8bbad71e06315a8763c4d04900f.js
+++ precache-manifest.24ddf8bbad71e06315a8763c4d04900f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b3b9f9a4e97cb2be106ddae8ce134816",
+    "revision": "e17ffb893acd5c1404b5e1790c4be1cd",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "554b733b6d9e33a24284",
-    "url": "/portfolio/static/css/main.d7a4670c.chunk.css"
+    "revision": "b3ec368c5cece9a48361",
+    "url": "/portfolio/static/css/main.42e1397d.chunk.css"
   },
   {
-    "revision": "90ee396e2fa2beca0799",
-    "url": "/portfolio/static/js/2.0c823ea5.chunk.js"
+    "revision": "215c4e2b4bdf1be375b3",
+    "url": "/portfolio/static/js/2.1942b657.chunk.js"
   },
   {
-    "revision": "554b733b6d9e33a24284",
-    "url": "/portfolio/static/js/main.85a18cdf.chunk.js"
+    "revision": "b3ec368c5cece9a48361",
+    "url": "/portfolio/static/js/main.4a8be1a6.chunk.js"
   },
   {
     "revision": "5029282666060c428c88",
@@ -22,6 +22,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5cdb9d41c1c4454e2af1f3dc5494b1fe",
     "url": "/portfolio/static/media/Office3.5cdb9d41.mp4"
+  },
+  {
+    "revision": "60e95aed3fd28b3638312d2253d0963b",
+    "url": "/portfolio/static/media/coming-soon.60e95aed.jpg"
   },
   {
     "revision": "593854e85d79592b3c7a416c3202eede",
