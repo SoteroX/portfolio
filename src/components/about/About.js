24dbb873a9a311434fd21fb5364f09402b5d6421
SoteroX <soterox13@gmail.com>
2019-08-19 15:13:23 -0500

    fixed misspelled word

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,8 +8,9 @@ export default function About() {
         I'm a professional Software Developer that has experience in creating
         Desktop and Web apps. I spent time creating responsive React UI
         components based off of wireframes that were provided to me by the
-        product designer. My job position also require me to create RESTful CRUD
-        API using packages like Express to interact with our MySQL databases.
+        product designer. My job position also required me to create RESTful
+        CRUD API using packages like Express to interact with our MySQL
+        databases.
       </p>
     </div>
   );
