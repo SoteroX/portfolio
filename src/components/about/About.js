946252c82f2f657eb7a262e2b62be8c878546a85
SoteroX <soterox13@gmail.com>
2019-10-29 09:46:14 -0500

    fixed mobile background image and edited about section info

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -10,11 +10,11 @@ function About() {
             <p className="about-text">
               I'm a Software Developer with professional experience using
               libraries and frameworks like React/Redux, Node/Express, MongoDB,
-              MYSQL, Electron, React-Native, and SASS. Beside working with those
-              tools I also have experience with creating RESTful APIs and
-              deploying apps to Heroku and Firebase. Some new technologies that
-              I fine interesting are AI/ML and Blockchain apps, along with an
-              interest in working with Python.
+              MYSQL, Electron, React-Native, and SASS. While working with this
+              tools, I created responsive React UI components and RESTful APIs
+              that were deployed to Heroku and/or Firebase. Some new
+              technologies that I find interesting are AI/ML and Blockchain
+              apps, along with an interest in working with Python.
             </p>
           </div>
         </div>
